de6762e2d115daf63e4982997a0e5ac790f0abbd
Davi Sodré <davi.sodreoficial@gmail.com>
2024-03-10 17:25:19 -0300

    Alterando personalidade da IA

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ openai.api_key = os.getenv("OPEN_AI_KEY")
 
 nome = "Unknown"
 
-mensagens = [{"role": "system", "content": f"Seu nome é {nome} você é uma inteligencia artificial cômica e sincera, seu criador se chama Davi"},]
+mensagens = [{"role": "system", "content": f"Seu nome é {nome} você é uma inteligencia artificial feita para educação de crianças. Por tanto responderá dúvidas e poderá contar histórias."},]
 
 def ask_gpt(mensagens, user_input):
     mensagens = mensagens + [{"role": "user", "content": user_input}]
